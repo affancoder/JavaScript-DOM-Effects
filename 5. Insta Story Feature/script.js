110314dcc5722233e4d4ead7d5a9bca1c5793f89
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2024-02-13 15:27:27 +0530

    Add files via upload

---
+++ 5. Insta Story Feature/script.js
@@ -0,0 +1,51 @@
+let arr = [
+  {
+    dp: "https://images.unsplash.com/photo-1542012252680-7b802edd2570?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D",
+    story:
+      "https://images.unsplash.com/photo-1522307837370-cc113a36b784?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1539784259147-c586d7ae2226?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8fHw%3D",
+    story:
+      "https://images.unsplash.com/photo-1602418146215-21ac6eaf7f95?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story:
+      "https://images.unsplash.com/photo-1626320349982-29be531c0388?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fHw%3D",
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1542596594-649edbc13630?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story:
+      "https://images.unsplash.com/photo-1468011749792-10026eb12caf?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE5fHx8ZW58MHx8fHx8",
+  },
+];
+
+let clutter = "";
+
+arr.forEach(function (elem, idx) {
+  clutter =
+    clutter +
+    `<div class="story">
+  <img id="${idx}" src="${elem.dp}" alt=""></img>
+</div>`;
+});
+
+console.log(clutter);
+
+document.querySelector("#storiyan").innerHTML = clutter;
+
+let storiyan = document.querySelector("#storiyan");
+
+storiyan.innerHTML = clutter;
+
+storiyan.addEventListener("click", function (dets) {
+  document.querySelector("#full-screen").style.display = "block";
+  document.querySelector("#full-screen").style.backgroundImage = `url(${
+    arr[dets.target.id].story
+  })`;
+
+  setTimeout(function () {
+    document.querySelector("#full-screen").style.display = "none";
+  }, 3000);
+});
